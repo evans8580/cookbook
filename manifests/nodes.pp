932edda7d0641eaff3a54f6b5da2be19b643940e
Junho Lee <evans8580@gmail.com>
2016-05-16 16:56:45 +0900

    nodes.app modified

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -1,5 +1,49 @@
-node 'controller', 'JUNHOLEE-M-W00U' {
-	file { '/tmp/hello':
-		content => "Hello, world\n",
-	}
+node 'demo' {
+  nginx::website { 'adorable-animals':
+    site_domain => 'adorable-animals.com',
+  }
+  nginx::website { 'adorable-animals-staging':
+    site_domain => 'staging.adorable-animals.com',
+  }
+  include useradd
+  include ssh
+  include execdemo
+  include backup
+  include cat-pictures
+
+  class { 'ntp':
+    server => 'us.pool.ntp.org',
+  }
+
+  file { '/tmp/test':
+    content => 'Zaphod Beeblebrox, this is a very large drink',
+  }
+
+  file { '/tmp/puppet.lastrun':
+    content => inline_template('<%= Time.now %>'),
+    backup  => false,
+  }
+}
+
+node 'demo2' {
+  nginx::website { 'amusing-animals':
+    site_domain => 'funny.adorable-animals.com',
+  }
+  nginx::website { 'dog-pictures':
+    site_domain => 'dog-pictures',
+  }
+  include useradd
+  include ssh
+  include execdemo
+  include backup
+  include cat-pictures
+
+  class { 'ntp':
+    server => 'us.pool.ntp.org',
+  }
+
+  file { '/tmp/puppet.lastrun':
+    content => inline_template('<%= Time.now %>'),
+    backup  => false,
+  }
 }
