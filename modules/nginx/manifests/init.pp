05270edfd8d52c9256b62a4d1cd9c2c91083ec11
Junho Lee <evans8580@gmail.com>
2016-05-16 16:51:10 +0900

    add a lot of files

---
+++ modules/nginx/manifests/init.pp
@@ -0,0 +1,21 @@
+# Manage nginx webserver
+class nginx {
+	package { 'apache2.2-common':
+		ensure	=> absent,
+	}
+
+	package { 'nginx':
+		ensure	=> installed,
+		require	=> Package['apache2.2-common'],
+	}
+
+	service { 'nginx':
+		ensure	=> running,
+		enable	=> true,
+		require	=> Package['nginx'],
+	}
+
+	file { '/etc/nginx/sites-enabled/default':
+		ensure	=> absent,
+	}
+}
