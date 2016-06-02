1aec80874b7eac415dd1432b0c998fd38a7fbe40
Junho Lee <evans8580@gmail.com>
2016-06-03 08:38:12 +0900

    Importing

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -1,50 +1,5 @@
-node 'demo' {
-  include puppet
-  nginx::website { 'adorable-animals':
-    site_domain => 'adorable-animals.com',
-  }
-  nginx::website { 'adorable-animals-staging':
-    site_domain => 'staging.adorable-animals.com',
-  }
-  include useradd
-  include ssh
-  include execdemo
-  include backup
-  include cat-pictures
-
-  class { 'ntp':
-    server => 'us.pool.ntp.org',
-  }
-
-  file { '/tmp/test':
-    content => 'Zaphod Beeblebrox, this is a very large drink',
-  }
-
-  file { '/tmp/puppet.lastrun':
-    content => inline_template('<%= Time.now %>'),
-    backup  => false,
-  }
-}
-
-node 'demo2' {
-  nginx::website { 'amusing-animals':
-    site_domain => 'funny.adorable-animals.com',
-  }
-  nginx::website { 'dog-pictures':
-    site_domain => 'dog-pictures',
-  }
-  include useradd
-  include ssh
-  include execdemo
-  include backup
-  include cat-pictures
-
-  class { 'ntp':
-    server => 'us.pool.ntp.org',
-  }
-
-  file { '/tmp/puppet.lastrun':
-    content => inline_template('<%= Time.now %>'),
-    backup  => false,
+node 'cookbook' {
+  file { '/tmp/hello':
+    content => "Hello, world\n",
   }
 }
