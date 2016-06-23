785138ed37094f665a9aeb6965e0089fc802041e
Evans Lee <evans8580@gmail.com>
2016-06-23 19:03:14 +0900

    Automated

---
+++ modules/puppet/files/pull-updates.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+cd /home/evans/puppet
+git pull && /usr/local/bin/papply
--- modules/puppet/manifests/init.pp
+++ modules/puppet/manifests/init.pp
@@ -3,4 +3,23 @@ class puppet {
     source => 'puppet:///modules/puppet/papply.sh',
     mode   => '0755',
   }
+
+  file { '/usr/local/bin/pull-updates':
+    source => 'puppet:///modules/puppet/pull-updates.sh',
+    mode   => '0755',
+  }
+
+  file { '/home/evans/.ssh/id_rsa':
+    source => 'puppet:///modules/puppet/ubuntu.priv',
+    owner  => 'evans',
+    mode   => '0600',
+  }
+
+  cron { 'run-puppet':
+    ensure  => 'present',
+    user    => 'evans',
+    command => '/usr/local/bin/pull-updates',
+    minute  => '*/10',
+    hour    => '*',
+  }
 }
